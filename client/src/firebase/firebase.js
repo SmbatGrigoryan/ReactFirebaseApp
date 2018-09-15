a546c8438d09316d3fda573e3201133eff677844
SmbatGrigoryan <smbatgrigoryan@gmail.com>
2018-09-15 18:46:09 +0400

    firebase

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,11 +1,16 @@
 import * as firebase from 'firebase';
-import keys from './config/keys';
+//import keys from './config/keys';
 
 
-
-
-
-const configSelf = {
+// const config = {
+//   apiKey: keys.FIREBASE_API_KEY,
+//   authDomain: keys.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: keys.FIREBASE_DATABASE_URL,
+//   projectId: keys.FIREBASE_PROJECT_ID,
+//   storageBucket: keys.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: keys.FIREBASE_MESSAGING_SENDER_ID
+// };
+const configToModify = {
   apiKey: "AIzaSyD7gPt20JfczAQXvON6c2jfSOLcd3dzuws",
   authDomain: "newreactapp.firebaseapp.com",
   databaseURL: "https://newreactapp.firebaseio.com",
@@ -15,18 +20,18 @@ const configSelf = {
 };
 
 const config = {
-  apiKey: keys.FIREBASE_API_KEY,
-  authDomain: keys.FIREBASE_AUTH_DOMAIN,
-  databaseURL: keys.FIREBASE_DATABASE_URL,
-  projectId: keys.FIREBASE_PROJECT_ID,
-  storageBucket: keys.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: keys.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyD7gPt20JfczAQXvON6c2jfSOLcd3dzuws",
+  authDomain: "newreactapp.firebaseapp.com",
+  databaseURL: "https://newreactapp.firebaseio.com",
+  projectId: "newreactapp",
+  storageBucket: "newreactapp.appspot.com",
+  messagingSenderId: "682040577963"
 };
 
-console.log(config)
 
 
 firebase.initializeApp(config);
+
 
 const db = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
